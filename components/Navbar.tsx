bc1c836a3e5decf4b192ca36258525fc62281141
khushwant10 <khushwantsanwalot.in@gmail.com>
2024-12-30 16:12:09 +0000

    commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,56 +17,60 @@ const Navbar = () => {
         Docs: "/docs",
     };
 
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://20.193.158.159";
+
     return (
         <nav className="w-full h-14 border border-r-0 border-l-0 border-neutral-800 relative">
             <div className="h-full shadow-sm flex items-center justify-between px-4">
                 <div className="flex items-center space-x-8">
-                    <Link className='font-semibold' href="/http://20.193.158.159/">
+                    <Link className='font-semibold' href="/" passHref>
                         Hexel Studio
                     </Link>
-                    {/* Desktop Navigation */}
                     <div className='hidden md:flex space-x-8'>
-                        {Object.keys(Navitems).map((key) => (
-                            <a href={Navitems[key]} key={key}>
+                        {Object.entries(Navitems).map(([key, value]) => (
+                            <Link href={value} key={key} passHref>
                                 {key}
-                            </a>
+                            </Link>
                         ))}
                     </div>
                 </div>
 
-                {/* Desktop Button */}
                 <div className="hidden md:block">
-                    <Button className='dark flex items-center gap-2'>
-                        Agent Studio
-                    </Button>
+                    <Link href={baseUrl}>
+                        <Button className='dark flex items-center gap-2'>
+                            Agent Builder
+                        </Button>
+                    </Link>
                 </div>
 
-                {/* Mobile Hamburger */}
                 <button
                     className="md:hidden p-2"
                     onClick={() => setIsOpen(!isOpen)}
+                    aria-label="Toggle menu"
                 >
                     <div className={`w-6 h-0.5 bg-current transition-all ${isOpen ? 'rotate-45 translate-y-1.5' : ''}`} />
                     <div className={`w-6 h-0.5 bg-current my-1 ${isOpen ? 'opacity-0' : ''}`} />
                     <div className={`w-6 h-0.5 bg-current transition-all ${isOpen ? '-rotate-45 -translate-y-1.5' : ''}`} />
                 </button>
 
-                {/* Mobile Menu */}
                 {isOpen && (
                     <div className="md:hidden absolute top-14 left-0 right-0 bg-white dark:bg-neutral-900 border-b border-neutral-800">
                         <div className="flex flex-col p-4 space-y-4">
-                            {Object.keys(Navitems).map((key) => (
-                                <a
-                                    href={Navitems[key]}
+                            {Object.entries(Navitems).map(([key, value]) => (
+                                <Link
+                                    href={value}
                                     key={key}
                                     onClick={() => setIsOpen(false)}
+                                    passHref
                                 >
                                     {key}
-                                </a>
+                                </Link>
                             ))}
-                            <Button className='dark flex items-center gap-2'>
-                                Agent Studio
-                            </Button>
+                            <Link href={baseUrl}>
+                                <Button className='dark flex items-center gap-2 w-full'>
+                                    Agent Builder
+                                </Button>
+                            </Link>
                         </div>
                     </div>
                 )}

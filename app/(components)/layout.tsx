fbf2b4e87d47b8b1acc391e21b948515c39b9e33
khushwant10 <khushwantsanwalot.in@gmail.com>
2025-01-04 16:31:30 +0000

    commit

--- app/(components)/layout.tsx
+++ app/(components)/layout.tsx
@@ -1,22 +1,22 @@
 import Navbar from '@/components/Navbar'
 import React from 'react'
 
-const layout = ({ Children }: { Children :React.ReactNode}) => {
-  return (
-      <div className='text-white w-full min-h-screen'>
-          {/* Fixed Navbar with matching padding */}
-          <div className='fixed top-0 left-0 right-0 z-50 bg-black
+const Layout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className='text-white w-full min-h-screen'>
+            {/* Fixed Navbar with matching padding */}
+            <div className='fixed top-0 left-0 right-0 z-50 bg-black
         px-4         /* Mobile padding */
         sm:px-6      /* Small screens */
         md:px-12     /* Medium screens */
         lg:px-16     /* Large screens */
         xl:px-56     /* Extra large screens */
       '>
-              <Navbar />
-          </div>
-{Children}
-      </div>
-  )
+                <Navbar />
+            </div>
+            {children}
+        </div>
+    )
 }
 
-export default layout
+export default Layout

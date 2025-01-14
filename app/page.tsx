e167251b23f8598e92ec9cac29fd66fdc1b92723
khushwant04 <khushwantsanwalot2004@outlook.com>
2025-01-14 15:54:34 +0000

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Hero from "@/components/Hero";
+import { SparklesPreview } from "@/components/sparkle-preview";
+
 
 export default function Home() {
   return (
     <div className="dark h-full w-full">
-      <Hero />
+      <SparklesPreview />
    </div>
   );
 }
